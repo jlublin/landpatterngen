e3a3e58822b27ce2948cbff9801bcc30dd0b5aa3
Joachim Lublin <joachim.lublin@gmail.com>
2016-05-14 19:40:12 +0200

    Merged target_sym_eagle and target_eagle with new functionality

--- target_eagle.py
+++ target_eagle.py
@@ -1,37 +1,148 @@
 #!/usr/bin/env python3
 
-def get_target(name):
-	return Eagle(name)
+def get_target():
+	return SYM_Eagle()
 
-class Eagle:
-	def __init__(self, name):
-		self.scr = '''\
-# Created by LandPatternGen
+sym_scale = 100
+sym_wire_scale = 16
 
-grid mm;
-set wire_bend 2;
+class SYM_Eagle:
 
-edit '{name}.pac';
-'''.format(name=name)
+	def __init__(self):
+
+		self.symbols = []
+		self.packages = []
+
+
+	def add_symbol(self, name):
+
+		self.current_symbol = { 'name': name, 'pins': [], 'lines': [] }
+		self.symbols.append(self.current_symbol)
+
+
+	def add_package(self, name):
+
+		self.current_package = { 'name': name, 'pads': [], 'lines': [], 'circles': [], 'rectangles': [] }
+		self.packages.append(self.current_package)
 
 
 	def output(self, path):
+
+		scr = '''\
+# Created by LandPatternGen
+
+set wire_bend 2;
+
+'''
+		for symbol in self.symbols:
+			scr += 'edit \'{name}.sym\';\n'.format(name=symbol['name'])
+			scr += 'grid mil\n'
+
+			scr += 'layer 94;\n'
+
+			for pin in symbol['pins']:
+				scr += self.gen_sym_pin(pin[0], pin[1])
+
+			current_layer = -1
+
+			for line in symbol['lines']:
+
+				if(line[0] != current_layer):
+					scr += 'layer {};\n'.format(line[0])
+
+				scr += self.gen_sym_line(line[1], line[2])
+
+			scr += '\n'
+
+		for package in self.packages:
+			scr += 'edit \'{name}.pac\';\n'.format(name=package['name'])
+			scr += 'grid mm\n'
+
+			for pad in package['pads']:
+				scr += self.gen_pac_pad(pad[2], pad[3], pad[4])
+
+			current_layer = -1
+
+			for line in package['lines']:
+
+				if(line[0] != current_layer):
+					scr += 'layer {};\n'.format(line[0])
+
+				scr += self.gen_pac_line(line[1], line[2])
+
+			for circle in package['circles']:
+
+				if(circle[0] != current_layer):
+					scr += 'layer {};\n'.format(circle[0])
+
+				scr += self.gen_pac_circle(circle[1], circle[2])
+
+			for rect in package['rectangles']:
+
+				if(rect[0] != current_layer):
+					scr += 'layer {};\n'.format(rect[0])
+
+				scr += self.gen_pac_rectangle(rect[1], rect[2])
+
+			scr += '\n'
+
 		f = open(path, 'w')
-		f.write(self.scr)
+		f.write(scr)
 		f.close
 
 
-	def add_pad(self, type, angle, size, pos, number):
+	def add_sym_pin(self, id, pos):
+
+		self.current_symbol['pins'].append([id, pos])
+
+
+	def gen_sym_pin(self, id, pos):
+		return 'pin \'{id}\' I/O None Point R0 Both 0 ({x} {y});\n'.format(id=id, x=pos[0]*sym_scale, y=pos[1]*sym_scale);
+
+
+	def add_pac_pad(self, type, angle, size, pos, number):
+
+		self.current_package['pads'].append([type, angle, size, pos, number])
+
+
+	def gen_pac_pad(self, size, pos, number):
 
 		# TODO: Types and angle
 
-		self.scr += '''\
-layer 1;
-smd '{id}' {w} {h} -0 R0 ({x} {y});
-'''.format(id=number, w=size[0], h=size[1], x=pos[0], y=pos[1]);
+		return 'smd \'{id}\' {w} {h} -0 R0 ({x} {y});\n'.format(id=number, w=size[0], h=size[1], x=pos[0], y=pos[1]);
 
 
-	def add_line(self, layer_name, width, vertices):
+	def add_sym_line(self, layer_name, width, vertices):
+
+		if(layer_name == 'Symbols'):
+			layer = 94
+		else:
+			layer = 98
+
+		self.current_symbol['lines'].append([layer, width, vertices])
+
+
+	def gen_sym_line(self, width, vertices):
+
+		wires = ''
+
+		for i in range(len(vertices)):
+			if(i+1 < len(vertices)):
+
+				if(vertices[i+1][0] == 'end'):
+					vertices[i+1] = vertices[0];
+
+				wires += 'wire {} ({} {}) ({} {});\n'.format(
+							width * sym_wire_scale,
+							vertices[i][0] * sym_scale,
+							vertices[i][1] * sym_scale,
+							vertices[i+1][0] * sym_scale,
+							vertices[i+1][1] * sym_scale)
+
+		return wires
+
+
+	def add_pac_line(self, layer_name, width, vertices):
 
 		if(layer_name == 'Courtyard'):
 			layer = 41
@@ -39,6 +150,11 @@ smd '{id}' {w} {h} -0 R0 ({x} {y});
 			layer = 21
 		else:
 			layer = 51
+
+		self.current_package['lines'].append([layer, width, vertices])
+
+
+	def gen_pac_line(self, width, vertices):
 
 		wires = ''
 
@@ -55,13 +171,10 @@ smd '{id}' {w} {h} -0 R0 ({x} {y});
 							vertices[i+1][0],
 							vertices[i+1][1])
 
-		self.scr += '''\
-layer {};
-{}
-'''.format(layer, wires)
+		return wires
 
 
-	def add_circle(self, layer_name, diameter, pos):
+	def add_pac_circle(self, layer_name, diameter, pos):
 
 		if(layer_name == 'Courtyard'):
 			layer = 41
@@ -70,13 +183,15 @@ layer {};
 		else:
 			layer = 51
 
-		self.scr += '''\
-layer {};
-circle ({:.3f} {:.3f}) ({:.3f} {:.3f});
-'''.format(layer, pos[0], pos[1], pos[0] + diameter, pos[1])
+		self.current_package['circles'].append([layer, diameter, pos])
 
 
-	def add_rectangle(self, layer_name, pos1, pos2):
+	def gen_pac_circle(self, diameter, pos):
+
+		return 'circle ({:.3f} {:.3f}) ({:.3f} {:.3f});\n'.format(pos[0], pos[1], pos[0] + diameter, pos[1])
+
+
+	def add_pac_rectangle(self, layer_name, pos1, pos2):
 
 		if(layer_name == 'Courtyard'):
 			layer = 41
@@ -85,7 +200,36 @@ circle ({:.3f} {:.3f}) ({:.3f} {:.3f});
 		else:
 			layer = 51
 
-		self.scr += '''\
-layer {};
-rect ({:.3f} {:.3f}) ({:.3f} {:.3f});
-'''.format(	layer, pos1[0], pos1[1], pos2[0], pos2[1])
+
+		self.current_package['rectangles'].append([layer, pos1, pos2])
+
+
+	def gen_pac_rectangle(self, pos1, pos2):
+
+		return 'rect ({:.3f} {:.3f}) ({:.3f} {:.3f});\n'.format(pos1[0], pos1[1], pos2[0], pos2[1])
+
+
+if(__name__ == '__main__'):
+
+	target = get_target()
+
+	target.add_symbol('Diode')
+
+	target.add_sym_pin('A', (-1, 0))
+	target.add_sym_pin('K', (2, 0))
+	target.add_sym_line('Symbols', 1, [(-1,0), (0,0)])
+	target.add_sym_line('Symbols', 1, [(0,0.5), (0,-0.5), (1,0), ('end',0)])
+	target.add_sym_line('Symbols', 1, [(1,0.5), (1,-0.5)])
+	target.add_sym_line('Symbols', 1, [(1,0), (2,0)])
+
+	target.add_package('DIOM2012')
+
+	target.add_pac_pad(0, 0, (1, 1), (-1, 0), 0)
+	target.add_pac_pad(0, 0, (1, 1), (1, 0), 1)
+	target.add_pac_line('Courtyard', 0.1, [(-2,-1), (-2,1), (2,1), (2,-1), ('end', 0)])
+	target.add_pac_circle('Silk', 0.1, (-1, 0.75))
+	target.add_pac_line('Silk', 0.1, [(-0.5,-0.5), (0.5,-0.5)])
+	target.add_pac_line('Silk', 0.1, [(-0.5,0.5), (0.5,0.5)])
+	target.add_pac_rectangle('Silk', (-0.5,-0.5), (0,0.5))
+
+	target.output('target_eagle_test.scr')
