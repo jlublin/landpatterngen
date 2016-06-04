3b3a7988cfc323a8b57eb8bf75a1474c4348787e
Joachim Lublin <joachim.lublin@gmail.com>
2016-06-04 09:59:30 +0200

    dual_row can now be used directly for custom packages

--- packages/dual_row.py
+++ packages/dual_row.py
@@ -1,13 +1,39 @@
 from tollen import TolLen
 
+'''
+Arguments: (E1) (D) e (E) (S) (L) (b) (E2 D2 c r) npin mark (deleted) (holes) lead_type
+holes = (d, x, y)
+If C X Y Z predefined, use them instead as custom device
+'''
+
+def get_package(part, design, process):
+	return DualRow(part, design, process)
+
 class DualRow:
 
-	# part containts L, b, S, E, e, E1, D, npin, deleted, mark
+	# part containts L, b, S, E, e, E1, D, npin, mark, deleted, holes
 	# design contaits J_H, J_T, J_S and CE
 	# process contains F and P
 	def __init__(self, part, design, process):
 
-		for c in ['L', 'b']:
+		if(isinstance(part, list)):
+			p = part
+			part = {	'E1': TolLen(float(p[0][0]), float(p[0][1])),
+						'D': TolLen(float(p[1][0]), float(p[1][1])),
+						'e': float(p[2]),
+						'E': TolLen(float(p[3][0]), float(p[3][1])),
+						'S': TolLen(float(p[4][0]), float(p[4][1])),
+						'L': TolLen(float(p[5][0]), float(p[5][1])),
+						'b': TolLen(float(p[6][0]), float(p[6][1])),
+						'npin': int(p[7]),
+						'mark': p[8],
+						'deleted': p[9],
+						'holes': p[10]}
+
+			import ipc7351
+			design = ipc7351.IPC7351[p[11]]['B']
+
+		for c in ['L', 'b', 'E1', 'e', 'D', 'npin']:
 			if(c not in part):
 				print('Missing "{}"'.format(c))
 				return
