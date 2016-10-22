6e0e16743ae785a7097f163ada249bf4d6c3a5fd
Joachim Lublin <joachim.lublin@gmail.com>
2016-10-22 14:19:26 +0200

    Added SVG holes

--- target_svg.py
+++ target_svg.py
@@ -8,7 +8,7 @@ def get_target():
 class SVG:
 	def __init__(self):
 		self.svg = ET.parse('skeleton.svg')
-
+		self.mmpx = 3.543307
 
 	def output(self, path):
 		self.svg.write(path)
@@ -42,15 +42,17 @@ class SVG:
 
 		for pad in package['pads']:
 			self.gen_pac_pad(pad)
+
+		for mnt_pad in package['mnt_pads']: # TODO, adding mnt_pads not done
+			self.gen_pac_mnt_pad(mnt_pad)
+
+		for hole in package['holes']:
+			self.gen_pac_hole(hole)
+
+		for line in package['lines']:
+			self.gen_pac_line(line)
+
 		if(0):
-			for mnt_pad in package['mnt_pads']:
-				self.gen_pac_mnt_pad(mnt_pad)
-
-			for hole in package['holes']:
-				self.gen_pac_hole(hole)
-
-			for line in package['lines']:
-				self.gen_pac_line(line)
 
 			for circle in package['circles']:
 				self.gen_pac_circle(circle)
@@ -85,14 +87,15 @@ class SVG:
 
 	def add_pac_line(self, layer, width, vertices):
 
-		self.package['holes'].append(
+		self.package['lines'].append(
 			{
 				'layer': layer,
-				'w': width,
+				'width': width,
 				'vertices': vertices
 			})
 
 	def gen_pac_pad(self, pad): # type, angle, size, pos, number
+
 		top_layer = self.svg.find('.//g[@id="Top"]')
 
 		# TODO: Types and angle
@@ -100,39 +103,52 @@ class SVG:
 		el = ET.SubElement(top_layer, 'rect')
 		el.set('style', 'fill:#ff0000;fill-opacity:1;stroke:none;stroke-width:10;stroke-linecap:square;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1')
 		el.set('id', 'pin_{}'.format(pad['number']))
-		el.set('width', '{}'.format(pad['size'][0]))
-		el.set('height', '{}'.format(pad['size'][1]))
-		el.set('x', '{}'.format(pad['pos'][0]))
-		el.set('y', '{}'.format(pad['pos'][1]))
+		el.set('width', '{}'.format(pad['size'][0]*self.mmpx))
+		el.set('height', '{}'.format(pad['size'][1]*self.mmpx))
+		el.set('x', '{}'.format(pad['pos'][0]*self.mmpx))
+		el.set('y', '{}'.format(pad['pos'][1]*self.mmpx))
 
 
-	def gen_line(self, layer_name, width, vertices):
+	def gen_pac_hole(self, hole):
 
-		layer = self.svg.find('.//g[@id="{}"]'.format(layer_name))
+		top_layer = self.svg.find('.//g[@id="Holes"]')
 
-		if(layer_name == 'Courtyard'):
+		circle = ET.SubElement(top_layer, 'circle')
+
+		circle.set('style', 'fill:#eeee00;fill-opacity:1;stroke:none;stroke-width:0.0;stroke-linecap:square;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"')
+
+		circle.set('cx', '{}'.format(hole['pos'][0]*self.mmpx))
+		circle.set('cy', '{}'.format(hole['pos'][1]*self.mmpx))
+		circle.set('r', '{}'.format(hole['d']/2*self.mmpx))
+
+
+	def gen_pac_line(self, line):
+
+		layer = self.svg.find('.//g[@id="{}"]'.format(line['layer']))
+
+		if(line['layer'] == 'Courtyard'):
 			color = '#e63a81'
-		elif(layer_name == 'Silk'):
+		elif(line['layer'] == 'Silk'):
 			color = '#111111'
 		else:
 			color = '#000000'
 
-		line = ET.SubElement(layer, 'path')
-		line.set('style', 'fill:none;fill-rule:evenodd;stroke:{color};stroke-width:{}mm;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1;stroke-miterlimit:4;stroke-dasharray:none'.format(width, color=color))
+		el = ET.SubElement(layer, 'path')
+		el.set('style', 'fill:none;fill-rule:evenodd;stroke:{color};stroke-width:{}mm;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1;stroke-miterlimit:4;stroke-dasharray:none'.format(line['width'], color=color))
 
 		pathdata = ''
 		first = True
 
-		for (x,y) in vertices:
+		for (x,y) in line['vertices']:
 			if(first):
-				pathdata += 'M ' + '{},{}'.format(x*3.543307,y*3.543307)
+				pathdata += 'M ' + '{},{}'.format(x*self.mmpx,y*self.mmpx)
 				first = False
 			elif(x == 'end'):
 				pathdata += ' z'
 			else:
-				pathdata += ' L ' + '{},{}'.format(x*3.543307,y*3.543307)
+				pathdata += ' L ' + '{},{}'.format(x*self.mmpx,y*self.mmpx)
 
-		line.set('d', pathdata)
+		el.set('d', pathdata)
 
 
 	def gen_circle(self, layer_name, diameter, pos):
@@ -149,9 +165,9 @@ class SVG:
 		circle = ET.SubElement(layer, 'circle')
 		circle.set('style', 'fill:#{color};fill-opacity:1;stroke:none;stroke-width:0.0;stroke-linecap:square;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"'.format(color=color))
 
-		circle.set('cx', '{}'.format(pos[0]*3.543307))
-		circle.set('cy', '{}'.format(pos[1]*3.543307))
-		circle.set('r', '{}'.format(diameter/2*3.543307))
+		circle.set('cx', '{}'.format(pos[0]*self.mmpx))
+		circle.set('cy', '{}'.format(pos[1]*self.mmpx))
+		circle.set('r', '{}'.format(diameter/2*self.mmpx))
 
 
 if(__name__ == '__main__'):
